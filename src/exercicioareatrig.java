6e452252fabcdd5208c477dc08d5dbc33d11b497
Lucas Augusto <lucasamgs@gmail.com>
2020-05-20 20:55:29 -0300

    Leitura da largura com validação

--- src/exercicioareatrig.java
+++ src/exercicioareatrig.java
@@ -1,8 +1,23 @@
-
+import java.util.Locale;
+import java.util.Scanner;
 public class exercicioareatrig {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double largura, altura,area,perimetro;
+		int opçao;
+		area = 0;
+		perimetro = 0;		
+		System.out.println("Digite a largura do retangulo:");
+		largura = sc.nextDouble();
+		while(largura <= 0) {
+			System.out.println("o valor deve ser positivo.Tente novamente");
+			largura = sc.nextDouble();
+		}
+		
+		sc.close();
 
 	}
 
