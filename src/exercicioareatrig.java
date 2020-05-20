6792d56e4c4b494ed24a5c98e2f7b75d9e1bddfa
Lucas Augusto <lucasamgs@gmail.com>
2020-05-20 20:57:33 -0300

    Programa finalizado

--- src/exercicioareatrig.java
+++ src/exercicioareatrig.java
@@ -16,6 +16,59 @@ public class exercicioareatrig {
 			System.out.println("o valor deve ser positivo.Tente novamente");
 			largura = sc.nextDouble();
 		}
+		System.out.println("Digite a altura do retangulo:");
+		altura = sc.nextDouble();
+		while(altura <= 0) { 
+			System.out.println("o valor deve ser positivo.Tente novamente");
+			altura = sc.nextDouble();
+		}
+		System.out.println("MENU:");
+		System.out.println("1 - Mostrar area");
+		System.out.println("2 - Mostrar perimetro");
+		System.out.println("3 - sair");
+		System.out.println("Digite uma opção:");
+		
+		opçao = sc.nextInt();
+		
+		while(opçao != 3) {
+			if (opçao == 1) {
+				area = largura * altura;
+		System.out.printf("AREA = %.2f%n", area);
+		System.out.println("");
+		System.out.println("MENU:");
+		System.out.println("1 - Mostrar area");
+		System.out.println("2 - Mostrar perimetro");
+		System.out.println("3 - sair");
+		System.out.println("Digite uma opção:");
+		opçao = sc.nextInt();}
+			else if (opçao == 2) { perimetro = (largura * 2) + (altura * 2) ;
+			System.out.printf("PERIMETRO = %.2f%n",perimetro);
+			System.out.println("");
+			System.out.println("MENU:");
+			System.out.println("1 - Mostrar area");
+			System.out.println("2 - Mostrar perimetro");
+			System.out.println("3 - sair");
+			System.out.println("Digite uma opção:");
+			opçao = sc.nextInt();}
+			 
+			else{
+				System.out.println("OPÇAO INVALIDA");
+				System.out.println("");
+				System.out.println("MENU:");
+				System.out.println("1 - Mostrar area");
+				System.out.println("2 - Mostrar perimetro");
+				System.out.println("3 - sair");
+				System.out.println("Digite uma opção:");
+				opçao = sc.nextInt();
+							
+				}
+		}
+				
+		
+		if(opçao == 3) {
+			System.out.println("FIM DO PROGRAMA!");
+		} 
+		
 		
 		sc.close();
 
